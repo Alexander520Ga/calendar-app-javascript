213406acc4c9bed5ddf5b3e981052a266a93f47a
Alexander520Ga <83433911+Alexander520Ga@users.noreply.github.com>
2022-04-18 10:23:13 -0600

    calendar app with javascript completed

---
+++ src/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CalendarApp } from './CalendarApp';
+
+import './styles.css';
+
+
+
+ReactDOM.render(
+    <CalendarApp />,
+  document.getElementById('root')
+);
+
+
+/*
+
+useful  iusfol
+trying    traing
+was       waz
+enjoyed   enyoid
+sharing   shering
+result risolt
+
+belong    belon
+promise   promes
+handle    jendol
+Quotient quoshent     
+shut      shat
+reason  ruisen
+
+parser  parser
+network network
+load  loud
+screwdrivers  skrudraiver
+telling teling
+music
+
+perform perfom
+chapter chapter
+single  singol
+span    spen
+typeof  taipof
+season
+
+whether   weder 
+branch    brentch    
+chosen    chousen
+shown     shoun
+outputs   autputs
+easy
+
+haiti     108
+guatemala 111
+nicaragua 130
+mexico    140   
+colombia  144
+venezuela 152
+
+haiti       108
+bolivia     105
+el salvador 110
+
+guatemala   111      
+honduras    124
+brasil      128
+nicaragua   130  
+mexico      140
+colombia    144
+venezuela   152
+
+proceeds  prousidz  
+beneath beniz   
+stored    stored
+failing   feilin
+occurs    acer
+husband   jasbend
+
+
+pib nominal (guatemala)    pib percapita( guatemala)     IDH(guatemala)   seguridad(guatemala)   territorio(nicaragua)
+
+educacion(guatemala)     infraestructura(guatemala)      salud(guatemala)   poblacion(guatemala) crecimiento(guatemala)
+
+poder militar(honduras)     turismo(guatemala)   esperanza de vida(honduras)
+
+mexico: 6
+costa rica: 7
+
+SamplePage
+linea 216, 231, 323
+
+
+*/
---
+++ src/types/types.js
@@ -0,0 +1,26 @@
+
+export const types = {
+
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+
+    eventSetActive: '[event] Set Active',
+    eventLogout:'[event] Set logout',
+
+    eventStartAddNew:'[event] start Add New',
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+    eventLoaded:'[event] Event loaded',
+
+    authChecking:'[auth] Checking login state',
+    authCheckingFinish:'[auth] Finish checking login state',
+    authStartLogin:'[auth] Start  login ',
+    authLogin:'[auth]  login',
+    authStartRegister:'[auth] Start Register',
+    authStartStartTokenRenew:'[auth] Start token renew',
+    authLogout:'[auth] Logout'
+
+}
